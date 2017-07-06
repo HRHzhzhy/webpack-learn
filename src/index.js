a08b13fa4bc89c77b4c083c1d8713b98b8abf456
张振勇 <zhangzy@missfresh.cn>
2017-07-06 14:39:49 +0800

    css loader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import _ from 'lodash'
+import './style.css'
 function component() {
   var element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   // lodash import from modules
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // style.css中定义的样式
+  element.classList.add('hello')
 
   return element;
 }
